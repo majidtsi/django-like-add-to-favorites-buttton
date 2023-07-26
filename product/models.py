1f51f83d848a2267fa61daa28a1717620920e3d9
majid <majidtsi3@gmail.com>
2023-07-26 16:21:16 +0200

    add all project

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=50)
+    like=models.ManyToManyField(User,blank=True)
+
+    class Meta:
+        verbose_name = ("Product")
+        verbose_name_plural = ("Products")
+    def __str__(self):
+        return self.name
